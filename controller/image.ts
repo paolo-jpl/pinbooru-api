71db7f1096585e1c17d4f1bfd08c64983d4cbc38
ludicorgi <jpaolo.lojo@gmail.com>
2024-09-10 04:31:37 +0800

    added create image function

--- controller/image.ts
+++ controller/image.ts
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import { pool } from "../server";
 
 export async function getAllImages(){
@@ -12,5 +13,35 @@ export async function getImageById(id: string){
      JOIN   "User" ON "User".id = "Image"."uploaderId"
      WHERE  "Image".id = $1`, 
     [id]);
+  return res.rows
+}
+
+//KNOWN ISSUE: error if string has unescaped ('), requires sanitization
+export async function createImage(
+  imgURL: string, 
+  uploaderId: string, 
+  description?: string, 
+  source?: string, 
+  published?: boolean,
+  id?: string,
+){
+  if(!id){ id = nanoid() }
+
+  //TODO: turn into function?
+  const values = [description, source, published].map((property) => {
+    if(!property) { 
+      return "DEFAULT"
+    } else if (typeof property === "string"){
+      return `'${property}'`;
+    } else {
+      return property;
+    };
+  });
+
+  const res = await pool.query(
+    `INSERT into "Image" ("id", "imgURL", "uploaderId", "description", "source", "published")
+     VALUES ($1, $2, $3, ${values[0]}, ${values[1]}, ${values[2]})
+     RETURNING *`, 
+    [id, imgURL, uploaderId]);
   return res.rows
 }
