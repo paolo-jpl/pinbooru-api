71eb9650cc83093c493ce483becd3bce21ba7379
ludicorgi <jpaolo.lojo@gmail.com>
2024-09-10 18:58:46 +0800

    add tags queries and tests

--- controller/tags.ts
+++ controller/tags.ts
@@ -1,6 +1,33 @@
+import { nanoid } from "nanoid";
 import { pool } from "../server";
+import { nullToDefault } from "../util/query";
 
 export async function getAllTags(){
   const res = await pool.query('SELECT * FROM "Tag"');
   return res.rows
+}
+
+export async function getTagByName(name: string){
+  const res = await pool.query(
+    `SELECT "Tag".id, "Tag"."name", "TagCategory"."name" AS "category"
+     FROM   "Tag"
+     JOIN   "TagCategory" ON "TagCategory".id = "Tag"."categoryId"
+     WHERE  "Tag"."name" = $1`, 
+    [name]);
+  return res.rows
+}
+
+export async function createTag(
+  name: string,
+  categoryId?: number,
+) 
+{
+  const values = nullToDefault([categoryId])
+
+  const res = await pool.query(
+    `INSERT into "Tag" ("name", "categoryId")
+    VALUES ($1, ${values[0]})
+    RETURNING *`, 
+    [name])
+  return res.rows;
 }
--- test/controller/tags.test.ts
+++ test/controller/tags.test.ts
@@ -1,12 +1,67 @@
 import { pool } from '../../server';
 import * as tagController from '../../controller/tags';
 
-describe('get all images', () => {
+//Start and rollback transaction for each test
+beforeEach(async () => {
+  await pool.query('BEGIN')
+});
+
+afterEach(async () => {
+  await pool.query('ROLLBACK')
+});
+
+describe('get all tags', () => {
   it('should return an array of length 13 (all rows)', async () => {
     const data = await tagController.getAllTags()
     expect(data.length).toBe(13)
+    expect(data[1].name).toBe("clothing")
+    expect(data[10].name).toBe("original_character")
   })
 });
+
+describe('get tag by name', () => {
+  describe('given tag name "mech"', () => {
+    it('should return tag obj of name "mech"', async () => {
+      const data = await tagController.getTagByName("mech")
+      expect(data.length).toBe(1)
+      expect(data[0]).toEqual({id: 3, name: "mech", category: "content"})
+    })
+  })
+
+  describe('given tag name that does not exist', () => {
+    it('should return array of length 0', async () => {
+      const data = await tagController.getTagByName("random")
+      expect(data.length).toBe(0)
+    })
+  })
+})
+
+//ASSUMPTION: Can not force assign tag id (autoincrement)
+describe('create tag', () => {
+  describe('given all tag details', () => {
+    it('should return object with matching details', async () => {
+      const data = await tagController.createTag("paint", 3)
+      expect(data[0]).toMatchObject({name: "paint", categoryId: 3})
+    })
+  })
+
+  describe('given no optional details', () => {
+    it('should return object with default values', async () => {
+      const data = await tagController.createTag("action")
+      expect(data[0]).toMatchObject({name: "action", categoryId: 1})
+    })
+  })
+
+  describe('given categoryId that does not exist', () => {
+    it('should throw FKey restraint error (23503)', async () => {
+      try{
+        await tagController.createTag("morning", 4)
+      } catch (e) {
+        expect(e).toMatchObject({code: "23503"});
+      }
+    })
+  })
+})
 
 afterAll(async () => {
   await pool.end();
