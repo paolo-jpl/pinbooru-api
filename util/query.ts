305327e6712da62b0440263bc993d9792c4c62e2
ludicorgi <jpaolo.lojo@gmail.com>
2024-09-11 02:22:41 +0800

    update util functions

--- util/query.ts
+++ util/query.ts
@@ -1,15 +1,27 @@
 //KNOWN ISSUE: error if string has unescaped ('), requires sanitization
 //test for object types like Date
 export function nullToDefault(arr: any[]){
-  const values = arr.map((property) => {
-    if(!property) { 
+  const values = arr.map((value) => {
+    if(value == null) { 
       return "DEFAULT"
-    } else if (typeof property === "string"){
-      return `'${property}'`;
+    } else if (typeof value === "string"){
+      return `'${value}'`;
     } else {
-      return property;
+      return value;
     };
   });
-
   return values;
+}
+
+export function setUpdateColumn(values: any[], colnames: string[]){
+  const query = values.map((value, index) => {
+    if(value == null) {
+      return ""
+    } else if (typeof value === "string"){
+      return `"${colnames[index]}" = '${value}'`;
+    } else {
+      return `"${colnames[index]}" = ${value}`;
+    };
+  }).filter((value) => value != "").join(',');
+  return query;
 }
