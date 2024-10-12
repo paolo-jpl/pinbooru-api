840623b8d7b55bded03cb72cbd2808881f8e8a0b
ludicorgi <jpaolo.lojo@gmail.com>
2024-10-12 19:25:03 +0800

    add new query building functions

--- util/query.ts
+++ util/query.ts
@@ -1,4 +1,5 @@
 const format = require('pg-format');
+
 export function setInsertColumns(values: any[], colnames: string[]){
   let columns: string[] = []
   let inputs: any[] = []
@@ -20,4 +21,22 @@ export function setUpdateColumns(values: any[], colnames: string[]){
       return format(`%I = %L`, colnames[index], value)
   }).filter((value) => value != "").join(',');
   return query;
+}
+
+export function setWhere(values: any[], colnames: string[]){
+  const query = values.map((value, index) => {
+    if(value == null || Number.isNaN(value)) 
+      return ""
+    else
+      return format(`AND %I = %L`, colnames[index], value)
+  }).filter((value) => value != "").join(' ');
+  console.log(query)
+  return query;
+}
+
+export function setOffset(limit: number | string = `ALL`, page: number){
+  if(typeof limit === "number" && page > 0) //if parameters are not default
+    page = (page - 1) * limit
+  else page = 0;
+  return page;
 }
